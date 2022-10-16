1a087664aeba3b8b23e148d0f21645b23c351891
jihee <jihee0825@gmail.com>
2022-10-16 10:51:21 +0900

    add API KEY

--- JS/weather.js
+++ JS/weather.js
@@ -1,7 +1,12 @@
+const API_KEY = "b2c787ed5a4b41f39453feab7ba51cc0";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You Live in", lat, lng);
+    const lon = position.coords.longitude;
+    console.log("You Live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    console.log(url);
+    fetch(url);
 }
 
 function onGeoError() {
