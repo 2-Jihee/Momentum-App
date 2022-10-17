3e26e344cd5e1d6a415b214314ef2b7398245c30
jihee <jihee0825@gmail.com>
2022-10-17 23:56:14 +0900

    add weather

--- JS/weather.js
+++ JS/weather.js
@@ -3,10 +3,15 @@ const API_KEY = "b2c787ed5a4b41f39453feab7ba51cc0";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You Live in", lat, lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    console.log(url);
-    fetch(url);
+    // console.log("You Live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText =data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
